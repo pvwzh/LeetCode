434a3994a090df9ee04b6b1f84268395c3f5bb17
pvwzh <pvwzh@outlook.com>
2016-09-04 11:58:54 +0800

    add new solution for 217 Contains Duplicate

--- 217_Contains_Duplicate/solution.rb
+++ 217_Contains_Duplicate/solution.rb
@@ -1,5 +1,7 @@
 # @param {Integer[]} nums
 # @return {Boolean}
+
+# solution1
 def contains_duplicate(nums)
   hash = Hash.new(0)
   for num in nums
@@ -12,10 +14,7 @@ def contains_duplicate(nums)
   return false
 end
 
-
-nums = [2]
-if contains_duplicate(nums)
-  p true
-else
-  p false
+# solution2
+def contains_duplicate2(nums)
+  return nums.length == nums.uniq.length ? false : true
 end
